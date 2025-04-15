20b2fa91386652956855582d6e2d5a4512a6a2a9
joaopedro266 <joao.pedronunes266@gmail.com>
2025-04-15 14:02:47 -0300

    dockerz

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,6 @@
+import 'reflect-metadata';
 
+import '@shared/typeorm';
 import cors from 'cors';    
 import routes from './routes';
 import express, { NextFunction, Request, Response } from  'express'; 
@@ -15,14 +17,14 @@ import AppError from './Errors/AppError';
 
 
 app.use((error: Error, request: Request, response : 
-Response, next: NextFunction)=>{ 
+Response, next: NextFunction):void=>{ 
 if(error instanceof AppError){ 
-return response.status(error.statusCode).json({ 
+response.status(error.statusCode).json({ 
 status: 'error', 
 message: error.message 
 }); 
 } 
-return response.status(500).json({ 
+response.status(500).json({ 
 status: 'error', 
 message: 'Internal server error' 
 }); 
